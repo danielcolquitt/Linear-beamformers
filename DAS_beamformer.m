e4ff512e88cf4753f195afe48673b5e3fdc4e9df
danielcolquitt <daniel@colquitt.xyz>
2023-08-14 17:16:04 +0100

    Added beam steering

--- DAS_beamformer.m
+++ DAS_beamformer.m
@@ -1,6 +1,6 @@
-function [ B , V ] = DAS_beamformer( S , f  , c , Z , th )
+function [ B , SR ] = DAS_beamformer( S , f  , c , Z , th , sth )
 %DAS_beamformer DAS beamformer (unweighted)
-%Outputs [ Beampattern , Phase Delays ]
+%Outputs [ Power spectrum , Steered response ]
 %   S   - A vector of signals recevied at the hydrophones located at Z
 %   k   - Target frequency
 %   c   - wavespeed
@@ -11,8 +11,12 @@ function [ B , V ] = DAS_beamformer( S , f  , c , Z , th )
 % Calculate wavenumber
 k = 2.*pi.*f./c;
 
+% Compute the steering vector (as column vector)
+E = exp( 1i.*( k*[ cos( sth ) , sin( sth ) ]*Z ) ).';
+
 % Preallocate output vector
 B = NaN.*ones( length( th ) , 1 );
+SR = B;
 
 % Estimate covariance matrix
 R = S*S'/size( S , 2 );
@@ -22,6 +26,7 @@ for thn = 1:length( th )
     V = exp( 1i.*( k*[ cos( th( thn ) ) , sin( th( thn ) ) ]*Z ) ).';
     % Beamform
     B( thn ) = abs( V'*R*V );
+    SR( thn ) = real( E'*V );
 end
 
 
