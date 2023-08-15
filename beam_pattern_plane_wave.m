b9a33fe384e775acf50e36f30a797f6a5b4061ae
danielcolquitt <daniel@colquitt.xyz>
2023-08-15 13:58:14 +0100

    Revert "Revert "Reverting changes""
    This reverts commit bd6d77356ceef03cdc0c6c3b5fd9dc2b0a512332.

--- beam_pattern_plane_wave.m
+++ beam_pattern_plane_wave.m
@@ -15,9 +15,15 @@ est_DoA = false;                        % Estimate DoA from max of metric
 
 % Plane wave source characteristics
 c  = 0.02e6;                            % speed of sound [m/s]
+<<<<<<< HEAD
 a  = [ 1 , .2 , .2];                      % Vector of wave amplitudes [m]
 f  = [ 0.02e6 , 0.02e6 , 0.02e6 ];      % Vector of frequencies [Hz]
 th = [ 30 , -20 , 60 ]*pi/180;          % Vector of direction of
+=======
+a  = [ 1 , 1 , 1];                   % Vector of wave amplitudes [m]
+f  = [ 1e6 , 1e6 , 1e6 ];      % Vector of frequencies [Hz]
+th = [ -10 , 5 , 30 ]*pi/180;          % Vector of direction of
+>>>>>>> parent of e4ff512 (Added beam steering)
                                         % arrivals [rad]
 
 % Array characteristics
@@ -25,9 +31,7 @@ sample_r = 1e6;                         % Sampling rate
 sample_N = 1e4;                         % Sampling window
 noise_a  = 0.04;                        % Amplitude of AWGN
 N        = 8;                           % Number of sensors
-target_f = 0.02e6;                      % Target frequency
-steering = th( 1 );                           % Steered bearing - set to zero
-                                        % for no steering
+target_f = 1e6;                      % Target frequency
 
 % Compute ancillary quantities
 target_l = c./target_f;                 % wavelength
@@ -58,10 +62,8 @@ fprintf( 'Number of bearing samples: %g\n' , length( Th ) );
 fprintf( 'Actual DoA: %g rad\n' , th);
 
 % Beamformer output
-%[ B , SR ] = DAS_beamformer( S , target_f , c , ...
-%    [ X ; Y ] , Th , steering) ;
-[ B , SR ] = MVDR_beamformer( S , target_f , c , ...
-    [ X ; Y ] , Th , steering );
+B = DAS_beamformer( S , target_f , c , [ X ; Y ] , Th );
+%B = MVDR_beamformer( S , target_f , c , [ X ; Y ] , Th );
 
 if est_DoA == true
     % Compute SPL and DoA
@@ -70,6 +72,7 @@ end
 
 % Metric and DoA estimation
 figure; hold on;
+<<<<<<< HEAD
 plot( Th , SR , 'LineWidth' , 1 );
 for thn = 1:length( th )
     plot( [ th( thn ) ; th( thn ) ] , ...
@@ -89,6 +92,26 @@ hold off;
 
 % Polar plot of Metrics
 SNR = SR;
+=======
+plot( Th , B , 'LineWidth' , 1 );
+for thn = 1:length( th )
+    plot( [ th( thn ) ; th( thn ) ] , ...
+        [ min( B ) ; max( B ) ] , 'Color' , '#D95319' , 'LineWidth' , 1 );
+end
+if est_DoA == true
+    plot( [ theta ; theta ] , ...
+        [ min( B ) ; max( B ) ] , 'Linewidth' , 1 , ...
+        'Color' , '#EDB120' , 'LineStyle' , '--' );
+    plot( [ pi - theta ; pi - theta ] , ...
+        [ min( B ) ; max( B ) ] , 'Linewidth' , 1 , ...
+        'Color' , '#EDB120' , 'LineStyle' , '--' );
+end
+axis( [ min( Th ) , max( Th ) , min( B ) , max( B ) ] );
+box on;
+hold off;
+
+% Polar plot of Metrics and DoA estimation
+>>>>>>> parent of e4ff512 (Added beam steering)
 figure;
 ax = polaraxes; hold on;
 polarplot( Th , SNR , 'LineWidth' , 1);
