2e10827cecc20144d54892c4978fba824a4bb77d
Finley Boulton <69048057+FinBoulton@users.noreply.github.com>
2023-09-11 12:47:36 +0100

    Update ESPRIT_DOA.m
    Signed-off-by: Finley Boulton <69048057+FinBoulton@users.noreply.github.com>

--- ESPRIT_DOA.m
+++ ESPRIT_DOA.m
@@ -1,11 +1,11 @@
 function E = ESPRIT_DOA(R, V, N, th, d)
-% ESPRIT_DOA A simple implementation of the esprit (Estimation of signal
-% parameters via rotational invariance techniques) beamformer for DoA 
+% ESPRIT_DOA A simple implementation of the ESPRIT (Estimation of signal
+% parameters via rotational invariance techniques) algorithm for DoA 
 % estimation. Works with Spatial smoothing. Returns estimated DoA value(s).
 %   R - Spatial covariance matrix
 %   V - Scanning vectors
 %   N - Number of Recivers; multi-source use SAL for spatial smoothing 
-%   th - Signal DoA; only need to know number of signals
+%   th - Signal DoA; need to know number of signals
 %   d - Element spacing
 
 % Sanity check of input
@@ -15,22 +15,21 @@ elseif size( R , 1 ) ~= size( V , 2 )
     error( 'Number of columns/rows in R should be equal to the number of columns in V');
 end
 
-% Compute SVD (Singular Value Decomposition) of R
-[U, S, V] = svd(R) ;
+% Generalised eigendecomposition of R (covariance matrix)
+[U, S, V] = svd(R) ; % Using Singular Value Decomposition
 
-% Left signular vectors corresponding to the source
-S = U(:, 1:length(th)) ;
+% Left signular vectors corresponding to the signal subspace
+F = U(:, 1:length(th)) ;
 
-% Partion submarticies 
-S1 = S(1:N-1,:) ;
-S2 = S(2:N, :) ;
+% Partion submatrices
+E1 = F(1:N-1,:) ;
+E2 = F(2:N, :) ;
 
-% Compute P via LS (Least Squares)
-P = S1\S2 ;
+% Compute P (Rotation Matrix) via LS (Least Squares) 
+P = E1\E2 ;
 
-% Find the angles of the Eigenvalues of P
-W = angle(eig(P)) / (2*pi*d) ;
-E = asind(-W) ;
+% Use argument of eigenvalues of P to find DOA (Deg)
+E = asind(-(angle(eig(P)) / (2*pi*d))) ;
 
 
 end
